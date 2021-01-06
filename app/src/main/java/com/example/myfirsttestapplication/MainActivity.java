2d058e3d896ccd2c2065e16a47690f156e2ab8b8
4yjo <27517870+4yjo@users.noreply.github.com>
2021-01-06 11:31:22 +0100

    added 2nd Sensor (Magnetic Field)

--- app/src/main/java/com/example/myfirsttestapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirsttestapplication/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mSensorManager; //create instance of sensor manager system service following https://developer.android.com/codelabs/advanced-android-training-sensor-data#2
     private Sensor mGyroscope;
     private TextView mGyroscopeText;
+    private Sensor mOtherSensor;
+    private TextView mOtherSensorText;
+
 
     //onCreate send data all the time, also when app is not running in the foreground > better use onStart
 
@@ -31,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mGyroscopeText = (TextView) findViewById(R.id.label_gyroscope);
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         String sensor_error = getResources().getString(R.string.error_no_sensor);
+        mOtherSensorText = (TextView) findViewById(R.id.label_otherSensor);
+        mOtherSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         if (mGyroscope == null){
             mGyroscopeText.setText(sensor_error);
@@ -50,20 +55,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
             mGyroscopeText = (TextView)findViewById(R.id.label_gyroscope);
         }
+
+        if (mOtherSensor != null){
+            mSensorManager.registerListener(this, mOtherSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            mOtherSensorText = (TextView)findViewById(R.id.label_otherSensor);
+        }
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this); // unregisters all registered Listeners
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-    //int sensorType = sensorEvent.sensor.getType(); //only necessary if > 1 sensor used
-    float currentValue = sensorEvent.values[0]; //?
-    mGyroscopeText.setText(getResources().getString(R.string.label_gyroscope, currentValue)); //?
+        int sensorType = sensorEvent.sensor.getType(); //only necessary if > 1 sensor used
+        float currentValue = sensorEvent.values[0];
+        switch (sensorType) {
+            case Sensor.TYPE_GYROSCOPE:
+                //float currentValue = sensorEvent.values[0];
+                mGyroscopeText.setText(getResources().getString(R.string.label_gyroscope, currentValue));
+                break;
+
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mOtherSensorText.setText(getResources().getString(R.string.label_otherSensor, currentValue));
+                break;
+        }
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
