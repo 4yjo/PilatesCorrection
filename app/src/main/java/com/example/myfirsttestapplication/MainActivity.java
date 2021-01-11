fd38af297f1eddd1601b7e08606679c00e5ae01e
4yjo <27517870+4yjo@users.noreply.github.com>
2021-01-11 18:19:29 +0100

    using accelerometer data added vibration feedback added colored button feedback

--- app/src/main/java/com/example/myfirsttestapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirsttestapplication/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
 import java.util.List;
 
@@ -22,6 +27,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView mGyroscopeText;
     private Sensor mOtherSensor;
     private TextView mOtherSensorText;
+    private Button mTiltLeft;
+    private Button mTiltFront;
+    private Button mTiltRight;
+    private Vibrator mVibrator;
+
 
 
     //onCreate send data all the time, also when app is not running in the foreground > better use onStart
@@ -35,7 +45,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         String sensor_error = getResources().getString(R.string.error_no_sensor);
         mOtherSensorText = (TextView) findViewById(R.id.label_otherSensor);
-        mOtherSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+       // mOtherSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        mOtherSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mTiltLeft = (Button) findViewById(R.id.button2);
+        mTiltFront = (Button) findViewById(R.id.button3);
+        mTiltRight = (Button) findViewById(R.id.button4);
+        mVibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
 
         if (mGyroscope == null){
             mGyroscopeText.setText(sensor_error);
@@ -55,6 +71,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onStart() {
         super.onStart();
 
+
         if (mGyroscope != null){
             mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
             mGyroscopeText = (TextView)findViewById(R.id.label_gyroscope);
@@ -64,6 +81,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             mSensorManager.registerListener(this, mOtherSensor, SensorManager.SENSOR_DELAY_NORMAL);
             mOtherSensorText = (TextView)findViewById(R.id.label_otherSensor);
         }
+
+
     }
 
     @Override
@@ -76,16 +95,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent sensorEvent) {
         int sensorType = sensorEvent.sensor.getType(); //only necessary if > 1 sensor used
 
+        float currentValueX = sensorEvent.values[0]; // 0 = X-Axis, 1 = Y-Axis, 2 = Z-Axis
+        float currentValueY = sensorEvent.values[1];
+        float currentValueZ = sensorEvent.values[2];
+
         switch (sensorType) {
             case Sensor.TYPE_GYROSCOPE:
-                float currentValueX = sensorEvent.values[0]; // 0 = X-Axis, 1 = Y-Axis, 2 = Z-Axis
-                float currentValueY = sensorEvent.values[1];
-                float currentValueZ = sensorEvent.values[2];
                 mGyroscopeText.setText(getResources().getString(R.string.label_gyroscope,
                         currentValueX, currentValueY, currentValueZ));
                 break;
 
-            case Sensor.TYPE_ROTATION_VECTOR:  //following https://code.tutsplus.com/tutorials/android-sensors-in-depth-proximity-and-gyroscope--cms-28084
+          /*  case Sensor.TYPE_ROTATION_VECTOR:  //following https://code.tutsplus.com/tutorials/android-sensors-in-depth-proximity-and-gyroscope--cms-28084
                 float[] rotationMatrix = new float[16];
                 SensorManager.getRotationMatrixFromVector(rotationMatrix, sensorEvent.values);
 
@@ -104,7 +124,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
                 mOtherSensorText.setText(getResources().getString(R.string.label_otherSensor, orientations[0], orientations[1], orientations[2]));
+                break;*/
+
+            case Sensor.TYPE_ACCELEROMETER:
+                mOtherSensorText.setText(getResources().getString(R.string.label_gyroscope,
+                        currentValueX, currentValueY, currentValueZ));
+
+                if (currentValueX  > 1.5) {
+                    // TODO: test what value is good (maybe > 2?)
+                    mTiltFront.setBackgroundColor(Color.GREEN);
+                    mVibrator.vibrate(50);
+
+                }
+                else if (currentValueX <-1.5){
+                    mTiltFront.setBackgroundColor(Color.BLUE);
+                    mVibrator.vibrate(50);
+                }
+                else{
+                    mTiltFront.setBackgroundColor(Color.YELLOW);
+                }
+
                 break;
+
+
+
         }
     }
 
