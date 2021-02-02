16f31abf341e71c1ff43ef3098a132cc748e1328
4yjo <27517870+4yjo@users.noreply.github.com>
2021-02-02 14:04:57 +0100

    set up TCP Client

--- app/src/main/java/com/example/myfirsttestapplication/connectToServer.java
+++ app/src/main/java/com/example/myfirsttestapplication/connectToServer.java
@@ -4,24 +4,49 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class connectToServer extends AsyncTask<Void, Void, Void> {
     //starts new class to connect to Server
     //if Permission is denied, make sure Server is running >> using server.pde Processing file for game
     //also make sure permission INTERNET is granted in Android Manifest
 
+    //using Dataoutputstream as in https://stackoverflow.com/questions/5680259/using-sockets-to-send-and-receive-data
+
     @Override
     protected Void doInBackground(Void... voids) {
-        try {
-            Socket mySocket = new Socket("192.168.0.200", 12345); //important: enter ip & port of Server here
-            Log.d("BBB", "successfully connected to socket");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("BBB", e.getMessage());
-        }
+        Log.d("AAA", "entered connectToServer");
+            try {
+                InetAddress address = InetAddress.getLocalHost();
+                Log.d("AAA", address.toString());
+            }
+            catch (UnknownHostException uhEx) {
+                Log.d("AAA", "unknown host exeption");
+            }
+            accessServer();
         return null;
     }
-}
 
+    private static void accessServer() {
+        Socket link = null;
+        try {
+            link = new Socket("192.168.0.200.", 12345); //important: put ip adress of server's host machine
+            OutputStream out = link.getOutputStream();
+            // byte[] data = ...
+            //out.write(data);  //send data to server
+            PrintWriter writer = new PrintWriter(out, true);
+            writer.println("Hello Server");
+            //TODO: put sensor data here instead
+
+
+        }
+        catch(IOException ioEx){
+            ioEx.printStackTrace();
+        }
+    }
+}
